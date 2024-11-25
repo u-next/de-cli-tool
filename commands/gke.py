4d88da284a3c8208b1cf2c470cc5c99c68d49345
Setsushin <x-zang@unext.jp>
2024-11-25 17:41:06 +0900

    add --filter for show-external-ip command, with case-insensitive match

--- commands/gke.py
+++ commands/gke.py
@@ -4,21 +4,45 @@ from utils.run_shell import run_shell
 
 @click.command()
 @click.argument("env")
-def show_external_ip(env):
+@click.option(
+    "--filter",
+    default="",
+    help="Filter services by name. Only services containing this string will be listed.",
+)
+def show_external_ip(env, filter):
     """
     List the external IP addresses of the services in the dev/prod GKE cluster \n
     Example: \n
-        show_external_ip dev \n
-        show_external_ip prod \n
+        show-external-ip dev \n
+        show-external-ip prod --filter kafka \n
     """
     if env not in ["dev", "prod"]:
         click.echo("Invalid environment. Please choose either 'dev' or 'prod'.")
         return
+
+    cluster_name = f"de-{env}-gke-cluster"
+    run_shell(
+        f"gcloud container clusters get-credentials {cluster_name} --zone asia-northeast1-a --project un-de-sawmill"
+    )
+
+    if filter:
+        kubectl_command = (
+            "kubectl get svc -A -o json | "
+            'jq -r \'["Namespace", "Name", "ExternalIP", "Ports"], '
+            f'(.items[] | select(.spec.type=="LoadBalancer" and (.metadata.name | test("{filter}"; "i"))) | '
+            "[.metadata.namespace, .metadata.name, "
+            '(.status.loadBalancer.ingress[0].ip // "None"), '
+            '(.spec.ports | map(.name + ":" + (.port | tostring)) | join(", "))]) | @tsv\' | '
+            "column -t"
+        )
     else:
-        cluster_name = f"de-{env}-gke-cluster"
-        run_shell(
-            f"gcloud container clusters get-credentials {cluster_name} --zone asia-northeast1-a --project un-de-sawmill"
+        kubectl_command = (
+            "kubectl get svc -A -o json | "
+            'jq -r \'["Namespace", "Name", "ExternalIP", "Ports"], '
+            '(.items[] | select(.spec.type=="LoadBalancer") | '
+            "[.metadata.namespace, .metadata.name, "
+            '(.status.loadBalancer.ingress[0].ip // "None"), '
+            '(.spec.ports | map(.name + ":" + (.port | tostring)) | join(", "))]) | @tsv\' | '
+            "column -t"
         )
-        run_shell(
-            'kubectl get svc -A -o json | jq -r \'["Namespace", "Name", "ExternalIP", "Ports"], (.items[] | select(.spec.type=="LoadBalancer") | [.metadata.namespace, .metadata.name, (.status.loadBalancer.ingress[0].ip // "None"), (.spec.ports | map(.name + ":" + (.port | tostring)) | join(", "))]) | @tsv\' | column -t'
-        )
+    run_shell(kubectl_command)
